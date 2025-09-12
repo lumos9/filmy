65679b710caa62f91cd2ae89cccecaf561227cb8
Coding Psycho <codingpsycho22@gmail.com>
2025-09-11 22:47:44 -0700

    changes

--- src/lib/analytics-store.ts
+++ src/lib/analytics-store.ts
@@ -1,6 +1,9 @@
 import { Redis } from "@upstash/redis";
 
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url: process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+});
 
 export async function getUniqueVisitorCount({ days = 1 }: { days: number }) {
   const today = new Date();
